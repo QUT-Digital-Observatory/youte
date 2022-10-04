58904e15dc8595c6a7546bebd4c803061e5a1bda
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2022-10-04 16:36:47 +1000

    use click.Path and add date string format validation

--- src/youte/utilities.py
+++ src/youte/utilities.py
@@ -1,13 +1,14 @@
 import logging
 from pathlib import Path
 import click
+import re
 
 from youte.exceptions import InvalidFileName
 
 logger = logging.getLogger(__name__)
 
 
-def validate_file(file_name, suffix=None):
+def validate_file(file_name: str, suffix=None):
     path = Path(file_name)
     if suffix and path.suffix != suffix:
         raise InvalidFileName("File name must end in `%s`." % suffix)
@@ -16,7 +17,8 @@ def validate_file(file_name, suffix=None):
 
 def check_file_overwrite(file_path: Path) -> Path:
     if file_path.exists():
-        flag = click.confirm(f"{file_path} already exists. Keep writing to it?")
+        flag = click.confirm(
+            f"{file_path} already exists. Keep writing to it?")
         if flag:
             logger.info(f"Updating existing file {file_path.resolve()}...")
         else:
@@ -28,3 +30,11 @@ def check_file_overwrite(file_path: Path) -> Path:
 
 def create_utc_datetime_string(string: str):
     return f"{string}T00:00:00Z"
+
+
+def validate_date_string(string: str) -> bool:
+    pattern = r"^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])$"
+    if re.match(pattern, string):
+        return True
+    else:
+        return False

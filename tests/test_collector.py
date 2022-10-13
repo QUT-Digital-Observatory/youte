3f9dcc7179a7244f855942f3c78997fd8622d0ee
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2022-10-13 17:30:32 +1000

    testing search command

---
+++ tests/test_collector.py
@@ -0,0 +1,148 @@
+import os
+import json
+import pytest
+import dotenv
+from click.testing import CliRunner
+
+from youte.config import YouteConfig
+from youte.collector import Youte
+from youte.__main__ import youte
+
+dotenv.load_dotenv()
+API_KEY = os.environ["STAGING_API_KEY"]
+NAME = "tester"
+
+
+@pytest.fixture()
+def config_path(tmp_path):
+    config_name = 'test_config'
+    return tmp_path / config_name
+
+
+def test_config_empty(config_path):
+    youte_conf = YouteConfig(filename=str(config_path))
+    assert not config_path.exists()
+
+
+def test_config_add(config_path):
+    youte_conf = YouteConfig(filename=str(config_path))
+    youte_conf.add_profile(NAME, API_KEY)
+
+    assert config_path.exists()
+
+
+@pytest.fixture()
+def youte_obj(config_path) -> Youte:
+    youte = Youte(api_key=API_KEY)
+
+    return youte
+
+
+@pytest.fixture()
+def runner() -> CliRunner:
+    runner = CliRunner()
+    yield runner
+    try:
+        os.remove('youte.log')
+    except FileNotFoundError:
+        pass
+
+
+def test_empty_youte(youte_obj):
+    assert youte_obj.api_key == API_KEY
+
+
+def test_search(youte_obj):
+    params = {
+        "part": "snippet",
+        "maxResults": 50,
+        "type": "video",
+        "order": "relevance"
+    }
+    search = youte_obj.search(query="mozart", **params)
+    page = 0
+    total_responses = 0
+
+    for results in search:
+        if page >= 3:
+            break
+        total_responses += len(results["items"])
+        page += 1
+
+    assert 50 < total_responses < 200
+
+
+@pytest.fixture()
+def search_params() -> dict:
+    params = {
+        "standard-no-output": ["search", "harry potter",
+                               "--from", "2021-01-01",
+                               "--to", "2021-02-01",
+                               "--key", API_KEY],
+        "standard-output": ["search", "harry potter",
+                            "--from", "2021-01-01",
+                            "--to", "2021-02-01",
+                            "--key", API_KEY,
+                            "output.json"],
+        "wrong-date-format": ["search", "harry potter",
+                              "--from", "10-01-2021",
+                              "--to", "2021-02-01",
+                              "--key", API_KEY],
+        "ordered-by-relevance": ["search", "harry potter",
+                                 "--from", "2021-01-01",
+                                 "--order", "relevance",
+                                 "--key", API_KEY],
+        "wrong-order-option": ["search", "harry potter",
+                               "--from", "2021-01-01",
+                               "--order", "alphabet",
+                               "--key", API_KEY],
+        "safe-search": ["search", "harry potter",
+                        "--from", "2021-01-01",
+                        "--safe-search", "moderate",
+                        "--key", API_KEY],
+        "long-videos": ["search", "harry potter",
+                        "--from", "2021-01-01",
+                        "--safe-search", "moderate",
+                        "--video-duration", "long",
+                        "--key", API_KEY]
+    }
+
+    return params
+
+
+@pytest.mark.parametrize("command",
+                         ["wrong-date-format",
+                          "wrong-order-option"])
+def test_cli_search_fail(runner, search_params, command):
+    results = runner.invoke(youte, search_params[command])
+    assert results.exception
+
+
+def test_cli_search_no_query(runner):
+    results = runner.invoke(youte, ['search', '--from', '2022-08-01',
+                                    '--key', API_KEY])
+    assert results.exception
+
+
+@pytest.mark.parametrize("command",
+                         ["standard-no-output",
+                          "ordered-by-relevance",
+                          "safe-search",
+                          "long-videos"])
+def test_cli_search(runner, search_params, command):
+    results = runner.invoke(youte, search_params[command])
+    assert results.exit_code == 0
+    assert 'youtube#searchListResponse' in results.output
+
+
+def test_cli_search_output(runner, tmp_path, search_params):
+    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+        results = runner.invoke(youte, search_params["standard-output"])
+        assert os.path.exists('output.json')
+
+        with open('output.json', 'r') as file:
+            row = file.readline()
+            r = json.loads(row)
+
+            assert len(r["items"]) > 10
+

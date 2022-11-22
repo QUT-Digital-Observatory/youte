69dc2ae4dde91c6c84d39d3f3a30225d5ddf34b6
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2022-11-22 12:07:14 +1000

    change youtupy to youte

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,8 @@
 import nox
 
+
 @nox.session
 def lint(session):
     session.install("flake8", "black")
-    session.run("flake8", "src/youtupy/")
-    session.run("black", "--check", "src/youtupy/")
+    session.run("flake8", "src/youte/")
+    session.run("black", "--check", "src/youte/")

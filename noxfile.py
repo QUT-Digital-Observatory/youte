f2f4a8470636be737ff87acaf0066bef8c4f8f2e
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2022-11-23 13:42:51 +1000

    add testing session

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,16 @@
 import nox
 
+nox.options.sessions = ["lint"]
+
 
 @nox.session
 def lint(session):
     session.install("flake8", "black")
     session.run("flake8", "src/youte/")
     session.run("black", "--check", "src/youte/")
+
+
+@nox.session(python=["3.8", "3.9", "3.10"])
+def test(session):
+    session.install("-r", "requirements.txt")
+    session.run("pytest", "tests")

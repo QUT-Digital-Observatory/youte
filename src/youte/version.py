5fdda5f8d97c68fd581b47c87c7fc8feb47a42fd
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2023-05-17 09:11:27 +1000

    bump version

---
+++ src/youte/version.py
@@ -0,0 +1,7 @@
+import platform
+
+version = "2.2.1"
+user_agent = (
+    f"youte/{version} ({platform.system()} {platform.machine()}) "
+    f"{platform.python_implementation()}/{platform.python_version()}"
+)

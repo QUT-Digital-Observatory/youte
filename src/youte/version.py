d7e772bf911316b968dbf9057a1508d906e5bfad
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2023-08-28 16:44:24 +1000

    bump minor release version

--- src/youte/version.py
+++ src/youte/version.py
@@ -1,6 +1,6 @@
 import platform
 
-version = "2.3.0b"
+version = "2.3.0"
 user_agent = (
     f"youte/{version} ({platform.system()} {platform.machine()}) "
     f"{platform.python_implementation()}/{platform.python_version()}"

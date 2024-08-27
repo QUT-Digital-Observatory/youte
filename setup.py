4ac068bf820f2da21c847a2346741abd20270f24
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2024-08-27 15:53:04 +1000

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
-__version__ = "2.6.0"
+__version__ = "2.6.1"
 
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
